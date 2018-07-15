0539b7d2802f80e7bd4f0e75718f838ec99b3faf
mohamedalikalil <41243142+mohamedalikalil@users.noreply.github.com>
2018-07-15 07:45:03 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=8;i++)
+    {
+       {
+
+        for(j=1;j<=8;j++)
+        {
+            printf("%d \t",i*j);
+        }
+        }
+        printf("\n");
+    }
+}
